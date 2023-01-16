ce769bd66ae34922513a88c00b3ad244a97a649f
rossenbergvillanuevaramboanga <rossenebrgvillanueva.ramboanga@gmail.com>
2023-01-16 10:02:52 +0100

    Create WebConfig

--- src/main/java/it/prova/gestioneroom/config/WebConfig.java
+++ src/main/java/it/prova/gestioneroom/config/WebConfig.java
@@ -1,4 +1,17 @@
 package it.prova.gestioneroom.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE");
+    }
+
 }
