f5676e4bcc68850d013db35ce9527d7e174976be
rossenbergvillanuevaramboanga <rossenebrgvillanueva.ramboanga@gmail.com>
2023-01-16 09:52:13 +0100

    Initial Commit

---
+++ src/main/java/it/prova/gestioneroom/GestioneRoomApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestioneroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestioneRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/prova/gestioneroom/GestioneRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.gestioneroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
